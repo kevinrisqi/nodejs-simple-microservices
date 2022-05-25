b9ad16a1d94b2add4c45d855920ba7012ccdb201
kevinrisqi <kevinrisqi22@gmail.com>
2022-05-25 18:08:49 +0700

    add criteria ios platform

--- controller/order.js
+++ controller/order.js
@@ -1,8 +1,8 @@
 const orderModel = require('../model/orderModel');
 
-const getAllOrder = async (req, reply) => {
+const addOrder = async (req, reply) => {
     try {
-        let result = await orderModel.test();
+        let result = await orderModel.definePlatform();
         reply.status(200).send(result);
     } catch (error) {
         reply.status(400).send(error);
@@ -10,5 +10,5 @@ const getAllOrder = async (req, reply) => {
 };
 
 module.exports = {
-    getAllOrder,
+    addOrder,
 }
--- model/orderModel.js
+++ model/orderModel.js
@@ -3,28 +3,61 @@ const { executeQuery } = require("../config/db");
 const status = { 1: "Menunggu antrian", 2: "Sedang dikerjakan", 3: "Selesai" };
 
 const ios = [
-    "Iphone 1",
+    "Iphone",
     "Iphone 2",
     "Iphone 3",
+    "Iphone 3 S",
+    "Iphone 3 G",
+    "Iphone 3GS",
     "Iphone 4",
+    "Iphone 4 G",
+    "Iphone 4 S",
     "Iphone 5",
+    "Iphone 5 S",
     "Iphone 6",
+    "Iphone 6 S",
+    "Iphone 6 Plus",
+    "Iphone 6 S+",
+    "Iphone 6 S Plus",
     "Iphone 7",
-    "Iphone X",
-    "Iphone 11",
-    "Iphone 12",
-    "Iphone 13",
+    "Iphone 7+",
+    "Iphone 7 Plus",
+    "Iphone 8",
+    "Iphone 8 Plus",
+    "Iphone 8 +",
+    "Iphone X Pro",
+    "Iphone 10",
     "Iphone XR",
+    "Iphone XS",
+    "Iphone 11",
+    "Iphone 11 Mini",
+    "Iphone 11 Pro",
+    "Iphone 11 Pro Max",
+    "Iphone 12",
+    "Iphone 12 Pro",
+    "Iphone 12 Pro Max",
+    "Iphone 12 Mini",
+    "Iphone 13",
+    "Iphone 13 Pro",
+    "Iphone 13 Pro Max",
+    "Iphone 13 Mini",
     "Ipad",
+    "Ipad Pro",
+    "Iphone SE 2016",
+    "Iphone SE 2020",
+    "Iphone SE 2021",
+    "Iphone SE 2022",
 ];
 
-const add = () => {
+const add = (payload) => {
     let result = executeQuery(
-        "INSERT INTO order (urutan, id_user, id_kerusakan, id_teknisi, status)"
+        "INSERT INTO order (nama, alamat, email, nomer_hp, brand_hp, platform, keluhan, antrian,status) VALUES (?,?,?,?,?,?,?,?,?)", [payload.nama, payload.alamat, payload.email, payload.nomer_hp, payload.brand_hp, payload.platform, payload.keluhan, payload.antrian, payload.status]
     );
+    return result;
 };
 
-const test = async () => {
+const definePlatform = async () => {
+
     let getBrand = await executeQuery("SELECT * FROM user WHERE id = ?", [5]);
 
     //TODO: Dummy Object
@@ -33,29 +66,25 @@ const test = async () => {
     //     { brand_hp: "iphone 7", platform: "ios", origin: "United States" }
     // ];
 
-    console.log(getBrand);
-
     //TODO: Mapping
     let brand = getBrand.map(res => {
         return res.brand_hp.toLowerCase();
     });
 
-    // let brand = ['ipad'];
+    // let brand = ['iphone 3'];
 
     //TODO: Define platform
     const platform = ios.find(item => brand[0] == item.toLowerCase()) ? 'IOS' : 'Android';
-    
-    
-    return platform;
 
+    return platform;
 
 };
 
-test().then(res => {
+definePlatform().then(res => {
     console.log(res);
 })
 
 module.exports = {
     add,
-    test,
+    definePlatform,
 };
