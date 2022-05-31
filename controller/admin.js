29eff356e0ff9d35cde9f7a2657caf84d610f24c
kevinrisqi <kevinrisqi22@gmail.com>
2022-05-31 19:13:31 +0700

    create controller to get all order on admin

---
+++ controller/admin.js
@@ -0,0 +1,10 @@
+const adminModel = require('../model/adminModel');
+
+const getAllOrder = async (req, reply) => {
+    try {
+        let result = 'test';
+        reply.status(200).send(result);
+    } catch (error) {
+        reply.status(400).send(error);
+    }
+}
--- model/adminModel.js
+++ model/adminModel.js
@@ -1,2 +1,7 @@
 const { executeQuery } = require('../config/db');
 
+const getOrder = async () => {
+    const queryOrder = await executeQuery("SELECT * FROM orders", []);
+
+    return queryOrder;
+}
