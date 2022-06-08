d224a787bdbc4dd92997e4ac46ec6df37395621e
kevinrisqi <kevinrisqi22@gmail.com>
2022-06-08 20:11:15 +0700

    create docs swagger for order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,9 @@
 const order = require('../controller/order');
+const docsOrder = require('../docs/swagger/docsOrder');
 
 async function routes(fastify) {
     fastify.post('/addOrder', order.addOrder);
-    fastify.get('/order', order.getAllOrder);
+    fastify.get('/order', docsOrder.findAllOrder, order.getAllOrder);
     fastify.get('/order/findByPhone/:phone_number', order.getOrderPhone);
     fastify.get('/order/findByStatus/:status', order.getOrderStatus);
     fastify.put('/order/changeStatusOrder/:id/:status', order.changeStatusOrder);
